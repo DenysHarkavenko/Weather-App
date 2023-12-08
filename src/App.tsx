28c4ffb4717d472d46920a4d3f9cd18bc4e418e9
Denys Harkavenko <garkavenkodenispost@gmail.com>
2023-12-08 21:44:14 +0100

    add basic elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Header } from './components/Header/Header';
 import './style/style.css'
+import { Card } from './components/Card/Card';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header/>
+      <main>
+        <Card />
+      </main>
     </div>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,11 +3,13 @@ import './_Header.css'
 export function Header() {
     return(
         <header className='header'>
-            <h1 className='header__title'>My Weather</h1>
-            <form>
-                <input type="text" placeholder="Enter the name of your city" />
-                <button type="submit">Show</button>
-            </form>
+            <div className="header-wrapper">
+                <h1 className='header__title'>My Weather</h1>
+                <form className='header__form'>
+                    <input className='header__form-input' type="text" placeholder="Enter the name of your city" />
+                    <button className='header__form-btn' type="submit">Show</button>
+                </form>
+            </div>
         </header>
     )
 }
